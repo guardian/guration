de289c15284627cba65d66ef0cab7ee7953efa6e
RichieAHB <richard.beddington@guardian.co.uk>
2018-06-05 12:20:21 +0100

    Fix broken logic

--- src/utils/FieldUtils.js
+++ src/utils/FieldUtils.js
@@ -14,6 +14,6 @@ const getChangedFields = (prevFields: Object, nextFields: Object) =>
       {}
     );
 
-const hasFields = (fields: Object) => Object.keys(fields).length === 0;
+const hasFields = (fields: Object) => Object.keys(fields).length !== 0;
 
 export { getChangedFields, hasFields };
