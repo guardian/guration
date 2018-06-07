b33ad87cc8d02146fd2443b41ea765f46a71ad4a
RichieAHB <richard.beddington@guardian.co.uk>
2018-06-07 13:47:02 +0100

    Add render children helper

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ export { default as Root } from './Root';
 export { default as Node } from './Node';
 export { default as Field } from './Field';
 export { default as Children } from './Children';
+export { default as RenderChildren } from './RenderChildren';
 export type { Edit, Move, Insert, Update } from './Edits';
