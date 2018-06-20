bd4adddb06b01aea3784c335e3f077cbb242bfe9
RichieAHB <richard.beddington@guardian.co.uk>
2018-06-20 13:52:45 +0100

    Add rollup-plugin-flow-defs

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import mkdirp from 'mkdirp';
-import fs from 'fs';
-import path from 'path';
-
-const flowStr = entry =>
-  `// @flow
-
-export * from '${entry}';
-`;
-
-const addFlowDefs = flowPath => ({
-  transformBundle: (_, id) => {
-    const file = `${id.file}.flow`;
-    mkdirp(path.dirname(file), err => {
-      if (err) {
-        throw `Couldn't write file`;
-      }
-      fs.writeFileSync(file, flowStr(flowPath));
-    });
-  }
-});
+import defs from 'rollup-plugin-flow-defs';
 
 export default [
   {
@@ -30,7 +10,7 @@ export default [
       file: 'dist/index.js',
       format: 'cjs'
     },
-    plugins: [babel(), addFlowDefs('../src/index.js')],
+    plugins: [babel(), defs()],
     external: ['react', 'lodash/fp/get', 'lodash/fp/set']
   },
 ];
