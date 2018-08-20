fca1caee44d60e59d5979da117db871249f9a29f
RichieAHB <richard.beddington@guardian.co.uk>
2018-08-20 16:13:23 +0100

    Remove root key from API

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -6755,6 +6755,85 @@
 
 	var lodash_throttle = throttle;
 
+	var rngBrowser = createCommonjsModule(function (module) {
+	// Unique ID creation requires a high quality random # generator.  In the
+	// browser this is a little complicated due to unknown quality of Math.random()
+	// and inconsistent support for the `crypto` API.  We do the best we can via
+	// feature-detection
+	// getRandomValues needs to be invoked in a context where "this" is a Crypto
+	// implementation. Also, find the complete implementation of crypto on IE11.
+	var getRandomValues = typeof crypto != 'undefined' && crypto.getRandomValues && crypto.getRandomValues.bind(crypto) || typeof msCrypto != 'undefined' && typeof window.msCrypto.getRandomValues == 'function' && msCrypto.getRandomValues.bind(msCrypto);
+
+	if (getRandomValues) {
+	  // WHATWG crypto RNG - http://wiki.whatwg.org/wiki/Crypto
+	  var rnds8 = new Uint8Array(16); // eslint-disable-line no-undef
+
+	  module.exports = function whatwgRNG() {
+	    getRandomValues(rnds8);
+	    return rnds8;
+	  };
+	} else {
+	  // Math.random()-based (RNG)
+	  //
+	  // If all else fails, use Math.random().  It's fast, but is of unspecified
+	  // quality.
+	  var rnds = new Array(16);
+
+	  module.exports = function mathRNG() {
+	    for (var i = 0, r; i < 16; i++) {
+	      if ((i & 0x03) === 0) r = Math.random() * 0x100000000;
+	      rnds[i] = r >>> ((i & 0x03) << 3) & 0xff;
+	    }
+
+	    return rnds;
+	  };
+	}
+	});
+
+	/**
+	 * Convert array of 16 byte values to UUID string format of the form:
+	 * XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+	 */
+	var byteToHex = [];
+
+	for (var i = 0; i < 256; ++i) {
+	  byteToHex[i] = (i + 0x100).toString(16).substr(1);
+	}
+
+	function bytesToUuid(buf, offset) {
+	  var i = offset || 0;
+	  var bth = byteToHex; // join used to fix memory issue caused by concatenation: https://bugs.chromium.org/p/v8/issues/detail?id=3175#c4
+
+	  return [bth[buf[i++]], bth[buf[i++]], bth[buf[i++]], bth[buf[i++]], '-', bth[buf[i++]], bth[buf[i++]], '-', bth[buf[i++]], bth[buf[i++]], '-', bth[buf[i++]], bth[buf[i++]], '-', bth[buf[i++]], bth[buf[i++]], bth[buf[i++]], bth[buf[i++]], bth[buf[i++]], bth[buf[i++]]].join('');
+	}
+
+	var bytesToUuid_1 = bytesToUuid;
+
+	function v4(options, buf, offset) {
+	  var i = buf && offset || 0;
+
+	  if (typeof options == 'string') {
+	    buf = options === 'binary' ? new Array(16) : null;
+	    options = null;
+	  }
+
+	  options = options || {};
+	  var rnds = options.random || (options.rng || rngBrowser)(); // Per 4.4, set bits for version and `clock_seq_hi_and_reserved`
+
+	  rnds[6] = rnds[6] & 0x0f | 0x40;
+	  rnds[8] = rnds[8] & 0x3f | 0x80; // Copy bytes to buffer, if provided
+
+	  if (buf) {
+	    for (var ii = 0; ii < 16; ++ii) {
+	      buf[i + ii] = rnds[ii];
+	    }
+	  }
+
+	  return buf || bytesToUuid_1(rnds);
+	}
+
+	var v4_1 = v4;
+
 	const RootContext = react.createContext({
 	  handleDragStart: (path, type) => e => {},
 	  handleDragOver: (path, getDuplicate, childInfo, getIndexOffset) => e => {},
@@ -7105,28 +7184,40 @@
 	const INTERNAL_TRANSFER_TYPE = '@@TRANSFER';
 
 	class Root extends react.Component {
-	  constructor(...args) {
-	    var _temp;
+	  constructor(props) {
+	    super(props);
 
-	    return _temp = super(...args), _defineProperty(this, "eventHandled", false), _defineProperty(this, "state", {
+	    _defineProperty(this, "rootKey", void 0);
+
+	    _defineProperty(this, "eventHandled", false);
+
+	    _defineProperty(this, "state", {
 	      dragData: null,
 	      dropInfo: {
 	        path: null,
 	        canDrop: false
 	      }
-	    }), _defineProperty(this, "runLowestOnly", fn => {
+	    });
+
+	    _defineProperty(this, "runLowestOnly", fn => {
 	      if (this.eventHandled) {
 	        return;
 	      }
 
 	      this.eventHandled = true;
 	      fn();
-	    }), _defineProperty(this, "handleDragStart", (path, type) => e => {
+	    });
+
+	    _defineProperty(this, "handleDragStart", (path, type) => e => {
 	      if (!e.dataTransfer) {
 	        return;
 	      }
 
+	      const {
+	        rootKey
+	      } = this;
 	      e.dataTransfer.setData(INTERNAL_TRANSFER_TYPE, JSON.stringify({
+	        rootKey,
 	        path,
 	        type
 	      })); // set this as we can't inspect dataTransfer on dragover
@@ -7134,16 +7225,21 @@
 	      this.setState({
 	        dragData: {
 	          dropType: 'MOVE',
+	          rootKey,
 	          path,
 	          type
 	        }
 	      });
-	    }), _defineProperty(this, "handleDragOver", (candidatePath, getDuplicate, childInfo, getIndexOffset) => e => {
+	    });
+
+	    _defineProperty(this, "handleDragOver", (candidatePath, getDuplicate, childInfo, getIndexOffset) => e => {
 	      this.runLowestOnly(() => {
 	        e.preventDefault();
 	        this.runDragOver(candidatePath, getDuplicate, childInfo, getIndexOffset, e);
 	      });
-	    }), _defineProperty(this, "runDragOver", lodash_throttle((candidatePath, getDuplicate, childInfo, getIndexOffset, e) => {
+	    });
+
+	    _defineProperty(this, "runDragOver", lodash_throttle((candidatePath, getDuplicate, childInfo, getIndexOffset, e) => {
 	      const indexOffset = getIndexOffset ? getIndexOffset(e) : 0;
 	      const path = addOffset(candidatePath, indexOffset);
 	      let edits = [];
@@ -7155,7 +7251,9 @@
 	      this.setDropInfo(path, !!edits.length);
 	    }, 100, {
 	      trailing: false
-	    })), _defineProperty(this, "handleDrop", (candidatePath, getDuplicate, childInfo, getIndexOffset) => e => {
+	    }));
+
+	    _defineProperty(this, "handleDrop", (candidatePath, getDuplicate, childInfo, getIndexOffset) => e => {
 	      this.runLowestOnly(() => {
 	        const {
 	          dataTransfer
@@ -7184,11 +7282,12 @@
 	            this.props.onChange(edits);
 	          }
 	        } catch (e) {
-	          console.log(e.message);
 	          this.props.onError(e.message);
 	        }
 	      });
-	    }), _temp;
+	    });
+
+	    this.rootKey = v4_1();
 	  }
 
 	  get dropMappers() {
@@ -7209,9 +7308,12 @@
 	      }
 	    }), {});
 	    return _objectSpread({}, insertMappers, {
-	      [INTERNAL_TRANSFER_TYPE]: data => _objectSpread({}, JSON.parse(data), {
-	        dropType: 'MOVE'
-	      })
+	      [INTERNAL_TRANSFER_TYPE]: data => {
+	        const json = JSON.parse(data);
+	        return _objectSpread({}, json, {
+	          dropType: this.rootKey === json.rootKey ? 'MOVE' : 'INSERT'
+	        });
+	      }
 	    });
 	  }
 
@@ -7394,24 +7496,6 @@
 	};
 
 	class Level extends react.Component {
-	  getDropProps(getDropProps, i) {
-	    const {
-	      arr,
-	      maxChildren
-	    } = this.props;
-	    return getDropProps(i, {
-	      childrenCount: arr.length,
-	      maxChildren
-	    });
-	  }
-
-	  renderDrop(i, getDropProps, isTarget) {
-	    const {
-	      renderDrop
-	    } = this.props;
-	    return !!renderDrop && renderDrop(this.getDropProps(getDropProps, i), isTarget(i), i);
-	  }
-
 	  render() {
 	    const {
 	      arr,
